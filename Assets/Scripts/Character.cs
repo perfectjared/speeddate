59daa62ef4d7a6652b6d2c288382199bd31ecd96
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-12-16 10:53:57 -0800

    Charcaters voices now change in Jared3

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -49,6 +49,7 @@ public class Character : MonoBehaviour
 
     public void Denitialize() {
         active = false;
+        GetComponent<RectTransform>().localPosition = new Vector3(-400, -30, 0);
         timeToSpeak = .1f;
     }
 
