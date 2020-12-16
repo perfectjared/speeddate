311f47bfb1e3d51776f8e7ff2eb27e5913b188bf
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-12-15 17:17:27 -0800

    Reaplaced Response bubbles with prefabs

---
+++ Assets/Scripts/Jacques/ResponseBubble.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponseBubble : MonoBehaviour
+{
+
+    private RectTransform rectTransform;
+    private Vector3 scaleTo = new Vector3(1.1f, 1.1f, 0);
+    private Vector3 scaleFrom = new Vector3(1f, 1f, 0);
+
+    private void OnGUI()
+    {
+        rectTransform = this.GetComponent<RectTransform>();
+    }
+
+    public void Click()
+    {
+        Responses.Instance.BubbleClicked(this.name);
+        Destroy(this.gameObject);
+    }
+    public void OnHover()
+    {
+        rectTransform.LeanScale(scaleTo, 0.2f);
+    }
+
+    public void OffHover()
+    {
+        rectTransform.LeanScale(scaleFrom, 0.2f);
+    }
+
+}
--- Assets/Scripts/Jacques/Responses.cs
+++ Assets/Scripts/Jacques/Responses.cs
@@ -2,54 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityTracery;
 
-public class Responses : MonoBehaviour
+public class Responses : Singleton<Responses>
 {
-    public RectTransform response_top, response_middle, response_bottom;
     public Texture[] bubbleColors;
+    public GameObject top, middle, bottom;
     public Vector3 topPos, middlePos, bottomPos;
+    public RectTransform spawnPos;
+    private Vector3 startPos = new Vector3(270,-200,0);
 
-    private Vector3 startPos = new Vector3(0,-250,0);
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        InitialiseResponses();
-    }
 
     // Hides the responses and resets thier rotation
     public void InitialiseResponses()
     {
-        response_bottom.localPosition = startPos;        
-        response_middle.localPosition = startPos;
-        response_top.localPosition = startPos;
+  
+        Debug.Log("Spawned");
+        GameObject newTop = top;
+        GameObject newMiddle = middle;
+        GameObject newBottom = bottom;
 
-        response_top.rotation = Quaternion.identity;
-        response_middle.rotation = Quaternion.identity;
+        if (!GameObject.Find("Top"))
+        {
+            newTop = Instantiate(newTop, spawnPos.position, Quaternion.identity, this.transform);
+            newTop.name = "Top";
+            newTop.LeanMoveLocal(topPos, 0.7f).setEaseSpring();
+            newTop.LeanRotateZ(-32, 1f).setEaseOutElastic();
+        }
+
+        if (!GameObject.Find("Middle"))
+        {
+            newMiddle = Instantiate(newMiddle, spawnPos.position, Quaternion.identity, this.transform);
+            newMiddle.name = "Middle";
+            newMiddle.LeanMoveLocal(middlePos, 0.7f).setEaseSpring();
+            newMiddle.LeanRotateZ(-16, 1f).setEaseOutElastic();
+        }
+
+        if (!GameObject.Find("Bottom"))
+        {
+            newBottom = Instantiate(newBottom, spawnPos.position, Quaternion.identity, this.transform);
+            newBottom.name = "Bottom";
+            newBottom.LeanMoveLocal(bottomPos, 0.5f).setEaseSpring();
+        }
     }
 
-    // Gives the responses a random image and displayes it on screen
-    public void ShowResponses()
+    public void BubbleClicked(string name) 
     {
-        //NOTE: I have no idea how we're gonna make the response bubbles be based on the message type
+        Debug.Log(name);        
+    }
 
-        var topImage = response_top.GetComponent<RawImage>();
-        var middleImage = response_middle.GetComponent<RawImage>();
-        var bottomImage = response_bottom.GetComponent<RawImage>();
-        
-        // Moves and rotates the top bubble
-        response_top.LeanMoveLocal(topPos, 0.7f).setEaseSpring();
-        response_top.LeanRotateZ(-32, 1f).setEaseOutElastic();
-        // Moves and rotates the middle bubble
-        response_middle.LeanMoveLocal(middlePos, 0.6f).setEaseSpring();
-        response_middle.LeanRotateZ(-16, 1f).setEaseOutElastic();
-        // Moves the bottom bubble
-        response_bottom.LeanMoveLocal(bottomPos, 0.5f).setEaseSpring();
+    // Shows three repsonses
+    public void Cycle()
+    {
+        //NOTE: I have no idea how we're gonna make the response bubbles be based on the message type     
 
-        // Randomly assigns a texture
-        topImage.texture = bubbleColors[Random.Range(0, 4)];
-        middleImage.texture = bubbleColors[Random.Range(0, 4)];
-        bottomImage.texture = bubbleColors[Random.Range(0, 4)];
+        //var topImage = response_top.GetComponent<RawImage>();
+        //var middleImage = response_middle.GetComponent<RawImage>();
+        //var bottomImage = response_bottom.GetComponent<RawImage>();
+
+        //// Moves and rotates the top bubble
+        //response_top.LeanMoveLocal(topPos, 0.7f).setEaseSpring();
+        //response_top.LeanRotateZ(-32, 1f).setEaseOutElastic();
+        //// Moves the middle button
+        //response_middle.LeanMoveLocal(middlePos, 0.7f).setEaseSpring();
+        //response_middle.LeanRotateZ(-16, 1f).setEaseOutElastic();
+        //// Moves the bottom bubble
+        //response_bottom.LeanMoveLocal(bottomPos, 0.5f).setEaseSpring();
+
+        //// Randomly assigns a texture
+        //topImage.texture = bubbleColors[Random.Range(0, 4)];
+        //middleImage.texture = bubbleColors[Random.Range(0, 4)];
+        //bottomImage.texture = bubbleColors[Random.Range(0, 4)];
     }
 }
